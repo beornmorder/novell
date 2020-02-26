7db690b74432a7fad99f1a8ccbd22c2e2bf98748
beorn morder <beorn.morder@outlook.com>
2020-02-26 14:12:35 -0600

    26-02-2020

--- labs/lab3/lab3-2.sh
+++ labs/lab3/lab3-2.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-#total distance: 1,000 miles
-#tank capacity: 18 gallons
-#cost per gallon: $3.40
-#data set: 18, 21, 24, 27, 32,36
-#input file = list of fuel efficenies
-#output file = chart including MPG, total trip cost, number of stops, and gas left in tank.
-
-#equations
-#number of stops = trip distance / miles per tank
-#miles per tank = tank capacity * MPG
-#cost of trip = number of stops * tank capacity * cost of gas
-#gas left = number of stops * tank ca
+#INPUT: any data file containing numbers
+#OUTPUT: stdout, can also be re-directed to file.
 
 #CONSTANTS
 totalDistance=1000
 tankCapacity=18
-galGasCost="340"
-fillCost=`expr galGasCost*tankCapacity`
+galGasCost="3.40"
 
  #first check if exactly one argument is given
 if [ "$#" -ne 1 ]; then #preferrably I'd loop thru each given arg. But I'm lazy.
@@ -25,22 +14,21 @@ if [ "$#" -ne 1 ]; then #preferrably I'd loop thru each given arg. But I'm lazy.
 elif [ ! -f $1 ]; then #check if argument is file. should work as full path or pwd.
   echo $1 "does not exist"
 else
-  echo -e "MPG \t Total Cost \t Stops \t Gas Left"
+  echo -e "MPG \t Total_Cost \t Stops \t Gas_Left"
   while read line #there is no reason to detect EOF. this is more efficent.
   do
       mpg=`echo $line | awk {'print $1'} | grep -o [[:digit:]]*`
       if [ ! -z $mpg ]; then
         tankDistance=$(($mpg*$tankCapacity))
-        stopCount=$(($totalDistance/$tankDistance))
+        stopCount=$(($totalDistance/$tankDistance+1)) #added 1 to represent starting with empty tank.
         totalGallons=$(($stopCount*$tankCapacity))
-        totalCost=$(($totalGallons*$galGasCost/100))
-        distanceCapacity=$((($stopCount+1)*$tankDistance))
+        totalCost=$(bc <<< "scale=2;$totalGallons*$galGasCost")
+        distanceCapacity=$((($stopCount)*$tankDistance))
         distanceLeft=$(($distanceCapacity-$totalDistance))
         gallonsLeft=$(($distanceLeft/$mpg))
         #i could have consolidated previous variables, but I prefer this approach
         #in case the program is re-purposed, it gives more re-usability.
-        echo -e $mpg "\t" $totalCost "\t\t" $stopCount "\t" $gallonsLeft
-        #csv or ncurses would be a better way to output data, but again, i'm lazy.
+        echo -e $mpg "\t" $totalCost "\t" $stopCount "\t" $gallonsLeft
       fi
    done < $1
 fi
---
+++ labs/lab3/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+variable=RANDOM
+echo $variable
